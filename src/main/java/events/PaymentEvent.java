e23a672489a78d9e00d5f6e56787db8de4f16d97
Siri <siriHome@hcmb0141204350.local>
2017-04-14 14:11:20 +0200

    edit logic

--- src/main/java/events/PaymentEvent.java
+++ src/main/java/events/PaymentEvent.java
@@ -1,7 +1,23 @@
 package events;
 
+import java.util.Date;
+
 /**
- * Created by siriHome on 14/04/2017.
+ * Created by sisaberi on 14/04/2017.
  */
 public class PaymentEvent {
+
+    int id;
+    double amount;
+    String ipAddress;
+    Date timestamp;
+
+    public PaymentEvent(int id,double amount,String ipAddres, Date timestamp){
+        this.id=id;
+        this.amount=amount;
+        this.ipAddress=ipAddres;
+        this.timestamp=timestamp;
+    }
+
+
 }
